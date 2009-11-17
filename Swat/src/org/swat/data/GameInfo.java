4a3f5764f01aa5312eb5a413b54c5f8b0e7678f0
tombuzbee <tombuzbee@gmail.com>
2009-11-17 01:28:10 +0000

    Data types

---
+++ Swat/src/org/swat/data/GameInfo.java
@@ -0,0 +1,29 @@
+package org.swat.data;
+
+import java.util.Map;
+
+public class GameInfo
+{
+	String gameName;
+	Map<String, Integer> pieceTypes;
+
+	public String getGameName()
+	{
+		return gameName;
+	}
+
+	public Map<String, Integer> getPieceTypes()
+	{
+		return pieceTypes;
+	}
+
+	public void setGameName(String gameName)
+	{
+		this.gameName = gameName;
+	}
+
+	public void setPieceTypes(Map<String, Integer> pieceTypes)
+	{
+		this.pieceTypes = pieceTypes;
+	}
+}
---
+++ Swat/src/org/swat/data/GameState.java
@@ -0,0 +1,84 @@
+package org.swat.data;
+
+import java.util.List;
+
+public class GameState
+{
+	List<String> currentMessages;
+	int gameID;
+	String gameType;
+	int grid[][];
+	int playTurnID;
+	boolean started;
+	String winnerID;
+
+	public List<String> getCurrentMessage()
+	{
+		return currentMessages;
+	}
+
+	public int getGameID()
+	{
+		return gameID;
+	}
+
+	public String getGameType()
+	{
+		return gameType;
+	}
+
+	public int[][] getGrid()
+	{
+		return grid;
+	}
+
+	public int getPlayTurnID()
+	{
+		return playTurnID;
+	}
+
+	public String getWinnerID()
+	{
+		return winnerID;
+	}
+
+	public boolean isStarted()
+	{
+		return started;
+	}
+
+	public void setCurrentMessages(List<String> currentMessages)
+	{
+		this.currentMessages = currentMessages;
+	}
+
+	public void setGameID(int gameID)
+	{
+		this.gameID = gameID;
+	}
+
+	public void setGameType(String gameType)
+	{
+		this.gameType = gameType;
+	}
+
+	public void setGrid(int[][] grid)
+	{
+		this.grid = grid;
+	}
+
+	public void setPlayTurnID(int playTurnID)
+	{
+		this.playTurnID = playTurnID;
+	}
+
+	public void setStarted(boolean started)
+	{
+		this.started = started;
+	}
+
+	public void setWinnerID(String winnerID)
+	{
+		this.winnerID = winnerID;
+	}
+}

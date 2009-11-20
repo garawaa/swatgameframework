ef42016972d3bbc8ea2476e7b3414be968ba6920
stvboy <stvboy@1485c124-cdb9-11de-8bde-2dfea787a966>
2009-11-20 01:09:40 +0000

    Updates.
    _steve.

--- trunk/Swat/src/org/swat/data/GameState.java
+++ trunk/Swat/src/org/swat/data/GameState.java
@@ -1,84 +1,115 @@
 package org.swat.data;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class GameState
-{
-	private List<String> currentMessages;
+import org.swat.server.game.GAME_STATE;
+import org.swat.server.game.Game;
+
+public class GameState {
+	
+	private int ID;
 	private int gameID;
-	private String gameType;
-	private int grid[][];
-	private int playTurnID;
-	private boolean started;
+	private int gameInstanceID;
+	
+	private int[][] pieceInfo;
+	private GAME_STATE gameState;
+	private String turnOfPlayer;
 	private String winnerID;
-
-	public List<String> getCurrentMessage()
-	{
-		return currentMessages;
+	private List<String> messages;
+	private ArrayList<String> players;
+	
+	private Game game;
+	
+	public GameState(Game game) {
+		this.game = game;
 	}
-
-	public int getGameID()
-	{
+	
+	/*
+	 * Property accessors
+	 */
+	public int getGameID() {
 		return gameID;
 	}
-
-	public String getGameType()
-	{
-		return gameType;
-	}
-
-	public int[][] getGrid()
-	{
-		return grid;
-	}
-
-	public int getPlayTurnID()
-	{
-		return playTurnID;
-	}
-
-	public String getWinnerID()
-	{
-		return winnerID;
-	}
-
-	public boolean isStarted()
-	{
-		return started;
-	}
-
-	public void setCurrentMessages(List<String> currentMessages)
-	{
-		this.currentMessages = currentMessages;
-	}
-
-	public void setGameID(int gameID)
-	{
+	public void setGameID(int gameID) {
 		this.gameID = gameID;
 	}
-
-	public void setGameType(String gameType)
-	{
-		this.gameType = gameType;
+	public int[][] getPieceInfo() {
+		return pieceInfo;
 	}
-
-	public void setGrid(int[][] grid)
-	{
-		this.grid = grid;
+	public void setPieceInfo(int[][] pieceInfo) {
+		this.pieceInfo = pieceInfo;
 	}
-
-	public void setPlayTurnID(int playTurnID)
-	{
-		this.playTurnID = playTurnID;
+	public GAME_STATE getGameState() {
+		return gameState;
 	}
-
-	public void setStarted(boolean started)
-	{
-		this.started = started;
+	public void setGameState(GAME_STATE gameState) {
+		this.gameState = gameState;
 	}
-
-	public void setWinnerID(String winnerID)
-	{
+	public String getTurnOfPlayer() {
+		return turnOfPlayer;
+	}
+	public void setTurnOfPlayer(String turnOfPlayer) {
+		this.turnOfPlayer = turnOfPlayer;
+	}
+	public String getWinnerID() {
+		return winnerID;
+	}
+	public void setWinnerID(String winnerID) {
 		this.winnerID = winnerID;
 	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public int getGameInstanceID() {
+		return gameInstanceID;
+	}
+	public void setGameInstanceID(int gameInstanceID) {
+		this.gameInstanceID = gameInstanceID;
+	}
+	public List<String> getMessages() {
+		return messages;
+	}
+	public void addMessage(String message) {
+		this.messages.add(message);
+	}
+	public void emptyMessages() {
+		this.messages.clear();
+	}
+	
+	public List<String> getPlayers() {
+		return players;
+	}
+	
+	public void addPlayer(String newPlayer) {
+
+		if(this.game.getNumberOfPlayersNeeded() > this.players.size())
+			this.players.add(newPlayer);
+		
+		if(this.game.getNumberOfPlayersNeeded() == this.players.size())
+			this.gameState = GAME_STATE.STARTED;
+
+	}
+	
+	public int getPlayerNumber(String playerUID) {
+		
+		int playerNumber = -1;
+		String[] playerUIDs = new String[players.size()];
+		playerUIDs = players.toArray(playerUIDs);
+		
+		for(int loop1=0; loop1<playerUIDs.length; loop1++)
+			if(playerUIDs[loop1].equals(playerUID))
+				playerNumber = loop1+1;
+		
+		return (playerNumber);
+		
+	}
+	
+	public synchronized void incrementStateID() {
+		this.ID++;
+	}
+	
 }
